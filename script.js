c2d65b690697cb4c6684e5e1ec4431d497dcd6aa
ClemDuss <56772098+ClemDuss@users.noreply.github.com>
2026-02-15 20:26:31 +0100

    add medals details page (for France)

--- script.js
+++ script.js
@@ -27,7 +27,100 @@ document.addEventListener('DOMContentLoaded', function() {
                 <span class="bronze">${totalMedals.bronze}</span>
             `;
 
+            if(country.organisation === 'FRA'){
+                // Load medals list fro France
+                let medalsDayByDay = [];
+                let allMedals = []
+                country.disciplines.forEach(someDiscipline => {
+                    someDiscipline.medalWinners.forEach(someMedal => {
+                        allMedals.push({
+                            discipline: someDiscipline.name,
+                            event: someMedal.eventDescription,
+                            medalType: someMedal.medalType,
+                            athlete: someMedal.competitorDisplayTvName,
+                            date: someMedal.date,
+                            disciplineCode: someDiscipline.code
+                        })
+                    })
+                })
+                allMedals.sort((a, b) => (a.discipline.localeCompare(b.discipline)));
+                allMedals.sort((a, b) => new Date(b.date) - new Date(a.date));
+                console.log(allMedals)
+
+                let lastDateRendered = null;
+                let lastDisciplineRendered = null;
+                let dayList = null;
+                
+                const medalsDetailsContainer = document.querySelector('.medals-details>.details-list');
+
+                allMedals.forEach(medal => {
+                    const someMedalDetails = document.createElement('div');
+                    someMedalDetails.classList.add('some-medal');
+
+                    // Check if date changes to reset discipline header and add date separator
+                    console.log(medal.date, lastDateRendered)
+                    if(lastDateRendered !== medal.date){
+                        const dateSeparator = document.createElement('div');
+                        dateSeparator.classList.add('day-header');
+                        if(medal.date == new Date().toISOString().split('T')[0]){
+                            dateSeparator.innerHTML = `<span>Aujourd'hui - ${new Date(medal.date).toLocaleDateString()}</span>`;
+                        } else {
+                            dateSeparator.innerHTML = `<span>${new Date(medal.date).toLocaleDateString()}</span>`;
+                        }
+                        medalsDetailsContainer.appendChild(dateSeparator);
+                        lastDateRendered = medal.date;
+                        lastDisciplineRendered = null
+                        dayList = document.createElement('div');
+                        dayList.classList.add('day-list');
+                        medalsDetailsContainer.appendChild(dayList);
+                    }
+
+                    // Check if discipline changes to add discipline separator
+                    if(lastDisciplineRendered !== medal.discipline){
+                        const disciplineSeparator = document.createElement('div');
+                        disciplineSeparator.classList.add('discipline-name');
+                        let disciplineIconUrl = `https://wmr-static-assets.scd.dgplatform.net/sm-cloudinary/Winter/Oly/Pictograms/SVG/Bold/${medal.disciplineCode.toLowerCase()}.svg`;
+                        disciplineSeparator.innerHTML = `
+                            <span class="icon" style="background-image: url(${disciplineIconUrl})"></span>
+                            ${medal.discipline}`;
+                        dayList.appendChild(disciplineSeparator);
+                        lastDisciplineRendered = medal.discipline;
+                    }
+
+                    // get medal emoji
+                    let medalEmoji = '🏅';
+
+                    switch(medal.medalType){
+                        case 'ME_GOLD':
+                            medalEmoji = '🥇';
+                            break;
+                        case 'ME_SILVER':
+                            medalEmoji = '🥈';
+                            break;
+                        case 'ME_BRONZE':
+                            medalEmoji = '🥉';
+                            break;
+                    }
+
+                    // Insert medal details in the container
+                    someMedalDetails.innerHTML = `
+                        <div class="medal-type">${medalEmoji}</div>
+                        <div class="medal-informations">
+                            <div class="athlete">${medal.athlete}</div>
+                            <div class="event-description">${medal.event}</div>
+                        </div>
+                    `;
+                    dayList.appendChild(someMedalDetails);
+
+                })
+            }
+
             medalsList.appendChild(countryItem);
         });
     });
 });
+
+
+document.querySelector('header .logo').addEventListener('click', ()=>{
+    document.querySelectorAll('.view').forEach(view => view.classList.toggle('displayed'));
+})
